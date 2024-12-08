679f2b88d3457730eee098205f863dec61a26f25
LarsNijholt <lars.nijholt@gmail.com>
2024-12-08 21:33:33 +0100

    Add proof of concept

--- src/bundles/ZyphCare.Web/Program.cs
+++ src/bundles/ZyphCare.Web/Program.cs
@@ -1,8 +1,12 @@
+using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using ZyphCare.Web.Components;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -11,7 +15,12 @@ services
     .AddInteractiveServerComponents();
 
 services
-    .AddMudServices();
+    .AddMudServices()
+    .AddAuth0WebAppAuthentication(options =>
+    {
+        options.Domain = configuration["Auth0:Domain"] ?? string.Empty;
+        options.ClientId = configuration["Auth0:ClientId"] ?? string.Empty;
+    });
 
 var app = builder.Build();
 
@@ -22,6 +31,25 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.MapGet("/Account/Login", async (HttpContext httpContext, string returnUrl = "/") =>
+{
+    var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+        .WithRedirectUri(returnUrl)
+        .Build();
+
+    await httpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+});
+
+app.MapGet("/Account/Logout", async httpContext =>
+{
+    var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+        .WithRedirectUri("/")
+        .Build();
+
+    await httpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+    await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+});
 
 app.UseHttpsRedirection();
 
