afa7c88ddb63570e252a626f6be1ba479890b6f8
LarsNijholt <lars.nijholt@gmail.com>
2024-12-14 13:07:41 +0100

    Add ZyphCare.Api.Common project with extensions and dependencies
    This commit introduces the ZyphCare.Api.Common project, including utility extensions for dictionaries, units, and Swagger integration. It incorporates key package dependencies like FastEndpoints and FastEndpoints.Swagger. The project has been added to the solution and properly configured for build and deployment.

---
+++ src/common-core/ZyphCare.Api.Common/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,49 @@
+using FastEndpoints.Swagger;
+using Microsoft.AspNetCore.Builder;
+using ZyphCare.Aspects.Contracts;
+
+namespace ZyphCare.Api.Common.Extensions;
+
+/// <summary>
+/// Extensions to enable the generation of Swagger API Documentation and associated Swagger UI.
+/// </summary>
+public static class SwaggerExtensions
+{
+    /// <summary>
+    /// Registers Swagger document generator.
+    /// </summary>
+    public static IUnit AddSwagger(this IUnit unit)
+    {
+        Version ver = new(3, 0);
+
+        // Swagger API documentation
+        unit.Services.SwaggerDocument(o =>
+        {
+            o.EnableJWTBearerAuth = true;
+            o.DocumentSettings = s =>
+            {
+                s.DocumentName = $"v{ver.Major}";
+                s.Title = "Elsa API";
+                s.Version = $"v{ver.Major}.{ver.Minor}";
+                s.AddAuth("ApiKey", new()
+                    {
+                        Name = "Authorization",
+                        In = NSwag.OpenApiSecurityApiKeyLocation.Header,
+                        Type = NSwag.OpenApiSecuritySchemeType.ApiKey,
+                        Description = "Enter: ApiKey [your API key]"
+                    });
+            };
+        });
+
+        return unit;
+    }
+
+    /// <summary>
+    /// Adds middleware to enable the Swagger UI at '/swagger'
+    /// </summary>
+    public static IApplicationBuilder UseSwaggerUI(this IApplicationBuilder app)
+    {
+        return app.UseSwaggerGen();
+    }
+
+}
