44cde8746b42937b7e40331118c8dbc3328e521d
LarsNijholt <lars.nijholt@gmail.com>
2025-02-16 15:58:29 +0100

    Move user and health record migrations to new SQLite structure
    Reorganized the user and health record database migrations for better modularity and clarity. This includes creating separate contexts for `UserZyphCare` and `HealthRecordZyphCare`. All table definitions, indices, and snapshot models have been adjusted accordingly.

---
+++ src/modules-core/ZyphCare.EntityFramework.Sqlite/Migrations/HealthRecordZyphCareDb/20250216144825_Initial_Create.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZyphCare.EntityFramework.Sqlite.Migrations.HealthRecordZyphCareDb
+{
+    /// <inheritdoc />
+    public partial class Initial_Create : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HealthRecords",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    PatientId = table.Column<string>(type: "TEXT", nullable: false),
+                    FileName = table.Column<string>(type: "TEXT", nullable: true),
+                    CreatedDate = table.Column<string>(type: "TEXT", nullable: false),
+                    Type = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HealthRecords", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HealthRecord_CreatedDate",
+                table: "HealthRecords",
+                column: "CreatedDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HealthRecord_FileName",
+                table: "HealthRecords",
+                column: "FileName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HealthRecord_Type",
+                table: "HealthRecords",
+                column: "Type");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HealthRecords");
+        }
+    }
+}
