54e39da46bc3b3f5a573101fcf15fc39f0df8ec4
LarsNijholt <lars.nijholt@gmail.com>
2025-02-16 15:58:34 +0100

    Add new API methods and enhance Blazor service documentation
    Introduced `GetWithAuth0IdAsync` and documentation for creating users in `IUserApi`. Additionally, added detailed summaries and remarks for `BlazorScopedProxyApi` and `BlazorServiceAccessor` classes to improve understanding and maintainability.

--- src/clients/ZyphCare.Api.Client/Users/Contracts/IUserApi.cs
+++ src/clients/ZyphCare.Api.Client/Users/Contracts/IUserApi.cs
@@ -19,10 +19,20 @@ public interface IUserApi
     /// <returns>A task that represents the asynchronous operation. The task result contains a paginated list response of users.</returns>
     [Get("/users")]
     Task<PagedListResponse<User>> ListAsync([Query] ListUserRequest request, CancellationToken cancellationToken = default);
-    
+
+    /// <summary>
+    /// Retrieves a user record based on the provided Auth0 identifier.
+    /// </summary>
+    /// <param name="authId">The Auth0 identifier of the user to retrieve.</param>
+    /// <param name="cancellationToken">A cancellation token that can be used to cancel the operation.</param>
     [Get("/users/by-auth-id/{authId}")]
     Task<IApiResponse<User?>> GetWithAuth0IdAsync([Query] string authId, CancellationToken cancellationToken = default);
-    
+
+    /// <summary>
+    /// Submits a request to create a new user with the specified details.
+    /// </summary>
+    /// <param name="request">The request containing the information required to create a user.</param>
+    /// <param name="cancellationToken">A cancellation token that can be used to cancel the operation.</param>
     [Post("/users")]
     Task PostAsync(PostUserRequest request, CancellationToken cancellationToken = default);
 }
