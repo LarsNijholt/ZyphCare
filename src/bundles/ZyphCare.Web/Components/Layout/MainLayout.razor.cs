47bd5f8fe4b0d1f955b01f44a75385154a27a89d
LarsNijholt <lars.nijholt@gmail.com>
2025-03-31 21:24:33 +0200

    Add drawer toggle and aspect initialization logic
    Introduced a drawer toggle functionality in `MainLayout.razor` via the `ToggleDrawer` method. Moved aspect initialization logic to `MainLayout.razor.cs` with authentication state checks, and removed redundant code from `Routes.razor`.

--- src/bundles/ZyphCare.Web/Components/Layout/MainLayout.razor.cs
+++ src/bundles/ZyphCare.Web/Components/Layout/MainLayout.razor.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
-using Syncfusion.Blazor.Navigations;
-using ZyphCare.Web.Models;
+using ZyphCare.Web.Core.Contracts;
+using ZyphCare.Web.Identity.Extensions;
 
 namespace ZyphCare.Web.Components.Layout;
 
@@ -8,4 +10,40 @@ public partial class MainLayout
 {
     private bool _drawerIsOpen = true;
     private ErrorBoundary? _errorBoundary;
+    
+    [Inject] private IAspectService AspectService { get; set; } = null!;
+
+    [CascadingParameter]
+    private Task<AuthenticationState>? AuthenticationState { get; set; }
+
+    /// <inheritdoc />
+    protected override async Task OnInitializedAsync()
+    {
+        if (AuthenticationState != null)
+        {
+            var authState = await AuthenticationState;
+            if (authState.User.Identity?.IsAuthenticated == true && !authState.User.Claims.IsExpired())
+            {
+                await AspectService.InitializeAspectsAsync();
+                StateHasChanged();
+            }
+        }
+        else
+        {
+            await AspectService.InitializeAspectsAsync();
+            StateHasChanged();
+        }
+        
+    }
+
+    /// <inheritdoc />
+    protected override void OnParametersSet()
+    {
+        _errorBoundary?.Recover();
+    }
+    
+    private void ToggleDrawer()
+    {
+        _drawerIsOpen = !_drawerIsOpen;
+    }
 }
